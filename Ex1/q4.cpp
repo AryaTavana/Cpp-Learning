e49a3a1c636a2a41b3b26969bd40a21b5d423d3a
AryaTavana <aryatavana07@gmail.com>
2026-09-02 19:46:00 +0330

    Implement event management system with create, delete, add, remove, and cost commands

--- Ex1/q4.cpp
+++ Ex1/q4.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Arya Tavana on 8/24/26.
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long a, b, c, d, m;
+    cin >> a >> b >> c >> d >> m;
+
+    long long redPrice = a + c * m;
+    long long greenPrice = b + d * m;
+
+    if ((redPrice > greenPrice && c > d) ||
+        (greenPrice > redPrice && d > c)) {
+        cout << "Eyval baba!\n";
+    } else {
+        cout << "Naaa, eshtebahe!\n";
+    }
+
+    return 0;
+}
--- Ex1/q5.cpp
+++ Ex1/q5.cpp
@@ -1,3 +1,77 @@
-//
-// Created by Arya Tavana on 8/24/26.
-//
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+    map<string, long long> costs;
+    map<string, set<string>> users;
+    string command;
+
+    while (cin >> command) {
+        if (command == "FINISH") {
+            break;
+        }
+
+        if (command == "CREATE") {
+            string eventName;
+            long long cost;
+            cin >> eventName >> cost;
+
+            if (costs.count(eventName)) {
+                cout << "UNSUCCESSFUL CREATE\n";
+            } else {
+                costs[eventName] = cost;
+                cout << "SUCCESSFUL\n";
+            }
+        } else if (command == "DELETE") {
+            string eventName;
+            cin >> eventName;
+
+            if (!costs.count(eventName)) {
+                cout << "INVALID EVENTNAME\n";
+            } else {
+                costs.erase(eventName);
+                users.erase(eventName);
+                cout << "SUCCESSFUL\n";
+            }
+        } else if (command == "ADD") {
+            string userName, eventName;
+            cin >> userName >> eventName;
+
+            if (!costs.count(eventName)) {
+                cout << "INVALID EVENTNAME\n";
+            } else if (users[eventName].count(userName)) {
+                cout << "USER ALREADY ADDED TO EVENT\n";
+            } else {
+                users[eventName].insert(userName);
+                cout << "SUCCESSFUL\n";
+            }
+        } else if (command == "REMOVE") {
+            string userName, eventName;
+            cin >> userName >> eventName;
+
+            if (!costs.count(eventName)) {
+                cout << "INVALID EVENTNAME\n";
+            } else if (!users[eventName].count(userName)) {
+                cout << "USER NOT FOUND IN EVENT\n";
+            } else {
+                users[eventName].erase(userName);
+                cout << "SUCCESSFUL\n";
+            }
+        } else if (command == "COST") {
+            string eventName;
+            cin >> eventName;
+
+            if (!costs.count(eventName)) {
+                cout << "INVALID EVENTNAME\n";
+            } else {
+                cout << costs[eventName] * users[eventName].size() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
